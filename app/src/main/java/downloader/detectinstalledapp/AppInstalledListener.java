383373f959a04a461f49043b5ca01c7c387c8b39
prerana Katyarmal <prerana_katyarmal@persistent.co.in>
2016-03-26 00:05:54 +0530

    Added another method to get installed package name.

--- app/src/main/java/downloader/detectinstalledapp/AppInstalledListener.java
+++ app/src/main/java/downloader/detectinstalledapp/AppInstalledListener.java
@@ -7,11 +7,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
-import java.util.logging.Logger;
+import java.util.List;
 
 /**
  * Created by prerana_katyarmal on 3/25/2016.
@@ -22,18 +22,33 @@ public class AppInstalledListener extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO Auto-generated method stub
         String action = intent.getAction();
-        if(action.equals("android.intent.action.PACKAGE_ADDED")){
-            String appNmae=getInstalledPackageInfo(context);
-            Log.v("appNmae:",appNmae);
+        if (action.equals("android.intent.action.PACKAGE_ADDED")) {
+            String appNmae = getRecentInstallPackageName(intent,context);
+            Log.v("appNmae:", appNmae);
         }
-        if(action.equals("android.intent.action.PACKAGE_REMOVED")){
-            Log.v("DATA:",intent.getData().toString());
+        if (action.equals("android.intent.action.PACKAGE_REMOVED")) {
+            Log.v("DATA:", intent.getData().toString());
         }
-        if(action.equals("android.intent.action.PACKAGE_REPLACED")){
-            Log.v("DATA:",intent.getData().toString());
+        if (action.equals("android.intent.action.PACKAGE_REPLACED")) {
+            Log.v("DATA:", intent.getData().toString());
         }
     }
 
+    private String getRecentInstallPackageName(Intent intent, Context context) {
+        String appName="";
+        String[] a = intent.getData().toString().split(":");
+        String packageName = a[a.length - 1];
+        List<PackageInfo> packageInfoList = context.getPackageManager().getInstalledPackages(0);
+        for (int i = 0; i < packageInfoList.size(); i++) {
+            PackageInfo packageInfo = packageInfoList.get(i);
+            if (packageInfo.packageName.equals(packageName)) {
+                appName = packageInfo.applicationInfo.loadLabel(context.getPackageManager()).toString();
+            }
+        }
+        return appName;
+    }
+
+//try this if above method did not work.
     private String getInstalledPackageInfo(Context context) {
         final PackageManager pm = context.getApplicationContext().getPackageManager();
         ApplicationInfo ai;
